315a45107701f5e705eb1e2a36448651d5313835
kwakhyun <khyun2348@naver.com>
2022-10-24 05:38:30 +0900

    Feat: Get now playing movies

---
+++ components/NavBar.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <StyledNavBar>
+      {/* <Image src="/vercel.svg" alt="logo" width={100} height={40} /> */}
+      <h1>Next Movies</h1>
+      <div>
+        <Link href={"/"}>
+          <a className={router.pathname === "/" ? "active" : ""}>현재 상영</a>
+        </Link>
+        <Link href={"/upcoming"}>
+          <a className={router.pathname === "/about" ? "active" : ""}>
+            개봉 예정
+          </a>
+        </Link>
+        <Link href={"/top"}>
+          <a className={router.pathname === "/movies" ? "active" : ""}>
+            최고 평점
+          </a>
+        </Link>
+      </div>
+    </StyledNavBar>
+  );
+}
+
+const StyledNavBar = styled.nav`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 0;
+  color: #fff;
+  background-color: #000;
+  font-family: "Gill Sans", sans-serif;
+  img {
+    max-width: 100px;
+    margin-bottom: 5px;
+  }
+  a {
+    font-weight: 600;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  a:hover {
+    color: #0070f3;
+  }
+  .active {
+    color: #0070f3;
+  }
+  div {
+    display: flex;
+    width: 100vw;
+    justify-content: space-around;
+  }
+`;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirect() {},
+  async rewrites() {
+    return [
+      {
+        source: "/movies/now",
+        destination: `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=ko-KR&page=1`,
+      },
+      {
+        source: "/movies/upcoming",
+        destination: `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=ko-KR&page=1`,
+      },
+      {
+        source: "/movies/top",
+        destination: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=ko-KR&page=1`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${process.env.API_KEY}&language=ko-KR`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+};
