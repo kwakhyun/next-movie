6514e997d4922098a265320e9c958938082fff58
kwakhyun <khyun2348@naver.com>
2022-10-24 07:23:35 +0900

    Fix: import error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Seo from "../components/Seo";
-import styled from "styled-components";
 
 export default function NotFound() {
   return (
-    <StyledNotFound>
+    <div>
       <Seo title="NOT FOUND" />
       <h1>페이지를 찾을 수 없습니다.</h1>
       <Link href="/">
@@ -12,21 +11,22 @@ export default function NotFound() {
           <h2>홈으로 돌아가기 &rarr;</h2>
         </a>
       </Link>
-    </StyledNotFound>
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        h1 {
+          font-size: 1.5rem;
+          margin-bottom: 1rem;
+        }
+        h2 {
+          font-size: 1.5rem;
+          color: #999;
+        }
+      `}</style>
+    </div>
   );
 }
-
-const StyledNotFound = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  h1 {
-    font-size: 1.5rem;
-    margin-bottom: 1rem;
-  }
-  h2 {
-    font-size: 1.5rem;
-    color: #999;
-  }
-`;
