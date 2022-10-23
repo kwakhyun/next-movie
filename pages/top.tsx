64d8c34dc74a229bce4a15b5d044739cf4c86417
kwakhyun <khyun2348@naver.com>
2022-10-24 06:52:00 +0900

    Add: upcoming page, top page

---
+++ pages/top.tsx
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function Top() {
+  return (
+    <div>
+      <Seo title="최고 평점" />
+      <h2>Top Rated 🎞</h2>
+    </div>
+  );
+}
---
+++ pages/upcoming.tsx
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function Upcoming() {
+  return (
+    <div>
+      <Seo title="개봉 예정" />
+      <h2>Upcoming Movies 🎥</h2>
+    </div>
+  );
+}
