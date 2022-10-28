89f20e51559a31ae0eb4223fd45de4ee78ff4818
kwakhyun <khyun2348@naver.com>
2022-10-29 04:30:56 +0900

    Feat: react query 도입

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,20 @@
 import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import NavBar from "./NavBar";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NavBar />
       <div>{children}</div>
-    </>
+    </QueryClientProvider>
   );
 }
