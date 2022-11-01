64fc7a00d2d8f8739845d41a3afb29c7afe918a0
kwakhyun <khyun2348@naver.com>
2022-11-01 19:28:35 +0900

    Remove: react query, axios

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,11 @@
 import { PropsWithChildren } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import NavBar from "./NavBar";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <NavBar />
       <div>{children}</div>
-    </QueryClientProvider>
+    </>
   );
 }
